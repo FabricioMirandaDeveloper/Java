4d552a542a7f42e736be2443bb87494ad7d74dd7
Fabricio <mirandaquezadafabriciojoel@gmail.com>
2024-07-01 16:59:33 -0500

    desarrollo de descuento en proceso

---
+++ Descuento.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Descuento {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingresa el precio del producto");
+        double productPrice = scanner.nextDouble();
+        double discount = 10;
+        if(productPrice >= 100) {
+
+        }
+    }
+}
